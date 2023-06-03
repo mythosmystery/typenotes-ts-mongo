cf6fd83dff59f5adb85398f900f50c588557533e
Hunter Barton <axiysfire@gmail.com>
2023-06-02 19:42:57 -0500

    feat: added testing

---
+++ src/db/db.test.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata'
+import { getDB, startDB } from '.'
+import { User } from '../models'
+
+test('startDB should return a client', async () => {
+  const client = await startDB()
+  expect(client).toBeDefined()
+  await client.close()
+})
+
+test('getDB should return a db', async () => {
+  const db = await getDB()
+  expect(db).toBeDefined()
+  expect(db.databaseName).toBe('app')
+})
+
+test('getCollection should return a collection', async () => {
+  const coll = await getDB().collection('test')
+  expect(coll).toBeDefined()
+})
+
+test('getCollection should return the correct collection', async () => {
+  const coll = await getDB().collection<User>('User')
+  expect(coll).toBeDefined()
+  expect(coll.collectionName).toBe('User')
+})
